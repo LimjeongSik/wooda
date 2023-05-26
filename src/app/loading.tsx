f454abae1f121f2ed13e47ba99359c418450c963
LimjeongSik <ol37@naver.com>
2023-05-27 00:01:55 +0900

    featrue : loading page and not found page

---
+++ src/app/loading.tsx
@@ -0,0 +1,10 @@
+import FullHeightInner from "@/components/Layout/FullHeightInner";
+import BlockTemplate from "@/components/Template/BlockTemplate";
+
+export default function Loading() {
+    return (
+        <FullHeightInner>
+            <BlockTemplate>로딩중...</BlockTemplate>;
+        </FullHeightInner>
+    );
+}
---
+++ src/app/not-found.tsx
@@ -0,0 +1,3 @@
+export default function NotFound() {
+    return <div>빈페이지</div>;
+}
